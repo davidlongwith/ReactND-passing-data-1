038752511e51e251e5671e75180bec1e9d693ea3
David <david@davidlongwith.com>
2020-11-13 01:23:09 +0000

    logging out

---
+++ src/FavoriteMovies.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FavoriteMovies extends Component {
+  render() {
+    console.log('Props', this.props)
+    return (
+      <ul>
+      </ul>
+    )
+  }
+}
+
+export default FavoriteMovies
